fe66c1fd915389cf87ab5b2efb48e0a1de7dbf46
ellahu <huqr20@mails.tsinghua.edu.cn>
2024-03-04 15:16:14 +0800

    initialize

---
+++ mine/template.py
@@ -0,0 +1,41 @@
+import rebound
+from numpy import pi as PI
+
+n_p = 100
+
+
+def hearbeat(sim_pointer):
+    pass
+
+sim = rebound.Simulation()
+
+# // Start the REBOUND visualization server. This
+# // allows you to visualize the simulation by pointing
+# // your web browser to http://localhost:1234
+# // The particles are tiny in this example. To see them
+# // on the screen press `s` to change their plotting style.
+
+sim.start_server(port=1234)
+
+# Simulation Setup
+# r->integrator    = REB_INTEGRATOR_MERCURIUS;
+sim.heartbeat = hearbeat
+sim.dt = 4./365.25*2.*PI # 4 days
+sim.integrator = 'whfast'
+sim.N_active = 2
+
+sim.add(m=1.)                # Sun
+sim.add(m=1, a=1., e=0.1) # Stellar flyby
+
+# test particles
+for i in range(n_p):
+    pass
+
+# move origin to COM
+sim.move_to_com()
+
+# integrate
+sim.integrate()
+
+# clean up
+sim = None
